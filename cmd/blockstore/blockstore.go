2259c38fd590dcdcfae2bbb8fe9e11be53010cd5
DeedleFake <deedlefake@users.noreply.github.com>
2019-08-09 16:40:40 -0400

    cmd/blockstore: Fix API breakage.

--- cmd/blockstore/blockstore.go
+++ cmd/blockstore/blockstore.go
@@ -8,42 +8,29 @@ import (
 	"path/filepath"
 
 	"github.com/DeedleFake/ventral/blockstore"
+	"github.com/restic/chunker"
 )
 
-func read(fs blockstore.FileSystem, args []string) {
-	if len(args) == 0 {
-		fmt.Fprintln(os.Stderr, "Error: no block IDs given")
-		os.Exit(2)
-	}
+func read(s blockstore.Store, args []string) {
+	for _, block := range args {
+		func() {
+			r, err := s.Read(block)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "Error: failed to open %q: %v\n", block, err)
+				os.Exit(1)
+			}
+			defer r.Close()
 
-	r, err := blockstore.Read(fs, args)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error: failed to open blockstore: %v\n", err)
-		os.Exit(1)
-	}
-	defer r.Close()
-
-	_, err = io.Copy(os.Stdout, r)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error: failed to read blocks: %v\n", err)
-		os.Exit(1)
+			_, err = io.Copy(os.Stdout, r)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "Error: failed to read %q: %v\n", block, err)
+				os.Exit(1)
+			}
+		}()
 	}
 }
 
-func write(fs blockstore.FileSystem, args []string) {
-	w := blockstore.Write(fs, blockstore.NewRabinChunker((1<<13)-1, 101))
-	defer func() {
-		err := w.Close()
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "Error: failed to write data: %v\n", err)
-			os.Exit(1)
-		}
-
-		for _, block := range w.Blocks() {
-			fmt.Println(block)
-		}
-	}()
-
+func concat(args []string) io.Reader {
 	if len(args) == 0 {
 		args = []string{"-"}
 	}
@@ -58,10 +45,68 @@ func write(fs blockstore.FileSystem, args []string) {
 		files = append(files, &lazyFile{path: arg})
 	}
 
-	_, err := io.Copy(w, io.MultiReader(files...))
+	return io.MultiReader(files...)
+}
+
+func write(s blockstore.Store, args []string) {
+	w, id, err := s.Write()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error: failed to create writer: %v\n", err)
+		os.Exit(1)
+	}
+	defer w.Close()
+
+	files := concat(args)
+
+	_, err = io.Copy(w, files)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error: failed to write data: %v\n", err)
 		os.Exit(1)
+	}
+
+	fmt.Printf("%x\n", id)
+}
+
+func chunk(s blockstore.Store, args []string) {
+	const pol = 0x3ffa0afcf682c7
+
+	files := concat(args)
+	c := chunker.New(files, pol)
+
+	var ids [][]byte
+	var buf []byte
+	for {
+		n, err := c.Next(buf[:0])
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+
+			fmt.Fprintf(os.Stderr, "Error: failed to create chunk: %v\n", err)
+			os.Exit(1)
+		}
+
+		func() {
+			w, id, err := s.Write()
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "Error: failed to create writer: %v\n", err)
+				os.Exit(1)
+			}
+			defer w.Close()
+
+			_, err = w.Write(n.Data)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "Error: failed to write chunk: %v\n", err)
+				os.Exit(1)
+			}
+
+			ids = append(ids, id)
+			buf = n.Data
+		}()
+	}
+
+	for _, id := range ids {
+		fmt.Printf("%x\n", id)
 	}
 }
 
@@ -69,24 +114,26 @@ func main() {
 	flag.Usage = func() {
 		fmt.Fprintf(os.Stderr, `Usage: %v [options] <command> <args>
 
+Options:
+	-r <path>
+		root of the blockstore (default $BLOCKSTORE_ROOT)
+
 Commands:
 	read <blocks...>
 		read blocks from the blockstore to stdout
 
 	write [files...]
 		write the data in files, or stdin if none are specified, to the
-		blockstore, then prints the IDs of the blocks written
+		blockstore as a single, concatenated block, then prints the ID of
+		the block written
 
-Options:
-	-r <path>
-		root of the blockstore (default $BLOCKSTORE_ROOT)
-
-	-gzip
-		use gzip compression
+	chunk [files...]
+		chunk writes the data in files, or stdin if none are specified, to
+		the blockstore in content-defined chunks, then prints the IDs of
+		the blocks written, in the order that they were written
 `, filepath.Base(os.Args[0]))
 	}
 	root := flag.String("r", os.Getenv("BLOCKSTORE_ROOT"), "root of the blockstore")
-	gzip := flag.Bool("gzip", false, "use gzip compression")
 	flag.Parse()
 
 	if *root == "" {
@@ -100,19 +147,21 @@ Options:
 		os.Exit(2)
 	}
 
-	cmd := map[string]func(blockstore.FileSystem, []string){
+	cmd := map[string]func(blockstore.Store, []string){
 		"read":  read,
 		"write": write,
+		"chunk": chunk,
 	}[mode]
 	if cmd == nil {
 		fmt.Fprintf(os.Stderr, "Error: unknown command: %q\n", mode)
 		os.Exit(2)
 	}
 
-	fs := blockstore.Dir(*root)
-	if *gzip {
-		fs = blockstore.Gzip(fs)
+	s, err := blockstore.Dir(*root)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error: failed to open the store: %v\n", err)
+		os.Exit(1)
 	}
 
-	cmd(fs, flag.Args()[1:])
+	cmd(s, flag.Args()[1:])
 }
