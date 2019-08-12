6b61a711835b5b9408701fcc64a85fcb10288565
DeedleFake <deedlefake@users.noreply.github.com>
2019-08-12 14:39:49 -0400

    ventral: Godoc.

--- ventral/client.go
+++ ventral/client.go
@@ -2,21 +2,35 @@ package ventral
 
 import (
 	"io"
+	"io/ioutil"
 	"net/http"
 	"net/url"
 	"path"
 )
 
-func Read(c *http.Client, u *url.URL, id string) (r io.ReadCloser, err error) {
-	uc := *u
+// Read sends a read request to a ventral server for the block with
+// the given ID. It returns a reader that reads the block's contents.
+// It is the caller's responsibility to close the reader.
+//
+// The URL to which the request is sent is built from the given URL by
+// adding only that which is necessary to make the request. It does so
+// without examining the existing URL, so the URL should point to the
+// root path of the API being contacted. In other words, if the API is
+// hosted under http://www.example.com/ventral/api, then that is the
+// URL which should be provided to this function.
+func Read(c *http.Client, apiURL string, id string) (r io.ReadCloser, err error) {
+	u, err := url.Parse(apiURL)
+	if err != nil {
+		return nil, err
+	}
 
-	uc.Path = path.Join(uc.Path, "read")
+	u.Path = path.Join(u.Path, "read")
 
-	q := uc.Query()
+	q := u.Query()
 	q.Set("id", id)
-	uc.RawQuery = q.Encode()
+	u.RawQuery = q.Encode()
 
-	rsp, err := c.Get(uc.String())
+	rsp, err := c.Get(u.String())
 	if err != nil {
 		return nil, err
 	}
@@ -24,11 +38,27 @@ func Read(c *http.Client, u *url.URL, id string) (r io.ReadCloser, err error) {
 	return rsp.Body, nil
 }
 
-func Write(c *http.Client, u *url.URL, r io.Reader) error {
-	uc := *u
+// Write sends a write request to a ventral server to create a new
+// block containing the data yielded by r. It returns the block's ID.
+//
+// The URL to which the request is sent is built from the given URL by
+// adding only that which is necessary to make the request. It does so
+// without examining the existing URL, so the URL should point to the
+// root path of the API being contacted. In other words, if the API is
+// hosted under http://www.example.com/ventral/api, then that is the
+// URL which should be provided to this function.
+func Write(c *http.Client, apiURL string, r io.Reader) (id []byte, err error) {
+	u, err := url.Parse(apiURL)
+	if err != nil {
+		return nil, err
+	}
+	u.Path = path.Join(u.Path, "write")
 
-	uc.Path = path.Join(uc.Path, "write")
+	rsp, err := c.Post(u.String(), "application/octet-stream", r)
+	if err != nil {
+		return nil, err
+	}
+	defer rsp.Body.Close()
 
-	_, err := c.Post(uc.String(), "application/octet-stream", r)
-	return err
+	return ioutil.ReadAll(rsp.Body)
 }
